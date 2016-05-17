2701393abbe11e3fe4286ebb8d050b8fd19f1fca
Vinhthuy Phan <vphan@memphis.edu>
2016-05-17 15:05:46 -0500

    .

--- src/handlers.go
+++ src/handlers.go
@@ -15,14 +15,8 @@ var Whiteboard string
 var WhiteboardExt string
 
 //-----------------------------------------------------------------
-
-func verifyPasscode(w http.ResponseWriter, r *http.Request) error {
-	if r.FormValue("passcode") != PASSCODE {
-		w.WriteHeader(http.StatusUnauthorized)
-		return errors.New("Unauthorized access")
-	}
-	return nil
-}
+// Student's handlers
+//-----------------------------------------------------------------
 
 //-----------------------------------------------------------------
 // users query to know their current points
@@ -53,21 +47,6 @@ func submit_postHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 //-----------------------------------------------------------------
-// return points of currently awarded users
-//-----------------------------------------------------------------
-func pointsHandler(w http.ResponseWriter, r *http.Request) {
-	if verifyPasscode(w, r) == nil {
-		js, err := json.Marshal(Points.data)
-		if err != nil {
-			fmt.Println(err.Error())
-		} else {
-			w.Header().Set("Content-Type", "application/json")
-			w.Write(js)
-		}
-	}
-}
-
-//-----------------------------------------------------------------
 // students receive broadcast
 //-----------------------------------------------------------------
 func receive_broadcastHandler(w http.ResponseWriter, r *http.Request) {
@@ -81,6 +60,18 @@ func receive_broadcastHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 //-----------------------------------------------------------------
+// Instructor's handlers
+//-----------------------------------------------------------------
+
+func verifyPasscode(w http.ResponseWriter, r *http.Request) error {
+	if r.FormValue("passcode") != PASSCODE {
+		w.WriteHeader(http.StatusUnauthorized)
+		return errors.New("Unauthorized access")
+	}
+	return nil
+}
+
+//-----------------------------------------------------------------
 // instructor broadcast contents to students
 //-----------------------------------------------------------------
 func broadcastHandler(w http.ResponseWriter, r *http.Request) {
@@ -88,6 +79,21 @@ func broadcastHandler(w http.ResponseWriter, r *http.Request) {
 		Whiteboard = r.FormValue("content")
 		WhiteboardExt = r.FormValue("ext")
 		fmt.Fprintf(w, "Content is saved to whiteboard.")
+	}
+}
+
+//-----------------------------------------------------------------
+// return points of currently awarded users
+//-----------------------------------------------------------------
+func pointsHandler(w http.ResponseWriter, r *http.Request) {
+	if verifyPasscode(w, r) == nil {
+		js, err := json.Marshal(Points.data)
+		if err != nil {
+			fmt.Println(err.Error())
+		} else {
+			w.Header().Set("Content-Type", "application/json")
+			w.Write(js)
+		}
 	}
 }
 
