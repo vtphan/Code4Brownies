fb76784b32a45abf83023cebd2d5cf11992910e6
vtphan <vphan@memphis.edu>
2016-05-25 20:38:22 -0500

    Version 0.20

--- src/compile.sh
+++ src/compile.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 env GOOS=darwin GOARCH=amd64 go build -o c4b_darwin_amd64
-env GOOS=windows GOARCH=amd64 go build -o c4b_windows_amd64
+env GOOS=windows GOARCH=amd64 go build -o c4b_windows_amd64.exe
