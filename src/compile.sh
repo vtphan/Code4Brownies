e7c3991399008f19e37da825a0b4479858bb8734
Vinhthuy Phan <vphan@memphis.edu>
2016-08-24 16:41:21 -0500

    add linux/amd64 server binary

--- src/compile.sh
+++ src/compile.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 env GOOS=darwin GOARCH=amd64 go build -o c4b_darwin_amd64
 env GOOS=windows GOARCH=amd64 go build -o c4b_windows_amd64.exe
+env GOOS=linux GOARCH=amd64 go build -o c4b_linux_amd64
