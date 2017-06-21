a0f1c8aba8a33649a0adf6fe5caa1a0614de6f1e
Vinhthuy Phan <vphan@memphis.edu>
2017-06-21 15:09:05 -0500

    separate io/db into new file.

---
+++ src/db.go
@@ -0,0 +1,81 @@
+//
+// Author: Vinhthuy Phan, 2015 - 2017
+//
+package main
+
+import (
+	"encoding/csv"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"strconv"
+	"time"
+)
+
+//-----------------------------------------------------------------
+func writeDB() {
+	var err error
+	var outFile *os.File
+	if _, err = os.Stat(USER_DB); err == nil {
+		outFile, err = os.OpenFile(USER_DB, os.O_APPEND|os.O_WRONLY, os.ModeAppend)
+	} else {
+		outFile, err = os.Create(USER_DB)
+	}
+	if err != nil {
+		panic(err)
+	}
+	defer outFile.Close()
+
+	t := time.Now()
+	fmt.Println(t.Format("Mon Jan 2 15:04:05 MST 2006: write data to ") + USER_DB)
+	w := csv.NewWriter(outFile)
+	for _, sub := range ProcessedSubs {
+		record := []string{sub.Uid, sub.Pid, strconv.Itoa(sub.Points), strconv.Itoa(sub.Duration), sub.Sid, sub.Pdes}
+		if err := w.Write(record); err != nil {
+			log.Fatalln("error writing record to csv:", err)
+		}
+	}
+	w.Flush()
+	if err := w.Error(); err != nil {
+		panic(err)
+	}
+}
+
+//-----------------------------------------------------------------
+func loadDB() map[string]*Submission {
+	var userFile *os.File
+	var err error
+
+	if _, err = os.Stat(USER_DB); os.IsNotExist(err) {
+		userFile, err = os.Create(USER_DB)
+		if err != nil {
+			panic(err)
+		}
+	} else {
+		userFile, err = os.Open(USER_DB)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+	defer userFile.Close()
+	reader := csv.NewReader(userFile)
+	entries := make(map[string]*Submission)
+	for {
+		record, err := reader.Read()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatal(err)
+		}
+		uid := record[0]
+		pid := record[1]
+		points, err := strconv.Atoi(record[2])
+		duration, err := strconv.Atoi(record[3])
+		sid := record[4]
+		s := &Submission{Uid: uid, Pid: pid, Points: points, Duration: duration, Sid: sid}
+		entries[sid] = s
+	}
+	return entries
+}
