2feaf76a230b2a65a07c275e8cc18a471f17826e
vtphan <vphan@memphis.edu>
2017-08-17 16:26:54 -0400

    .

--- src/main.go
+++ src/main.go
@@ -79,7 +79,7 @@ func AutoRegister(fn func(http.ResponseWriter, *http.Request)) http.HandlerFunc 
 func main() {
 	SERVER = informIPAddress()
 	fmt.Println("Server address:", "http://"+SERVER)
-	fmt.Println("Server must be run on the same machine with which codes are shared in SublimeText.")
+	fmt.Println("Run server on the same machine with which code is shared.")
 
 	rand.Seed(time.Now().UnixNano())
 	USER_DB = filepath.Join(".", "C4B_DB.csv")
